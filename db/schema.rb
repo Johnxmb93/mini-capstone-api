78e298dd4b5f4e2558a8a2c9803d33bec1c67b7f
Jonathan Bahena <bahena@Jonathans-MacBook-Air.local>
2021-06-25 13:35:19 -0500

    added 2 supppliers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_25_174956) do
+ActiveRecord::Schema.define(version: 2021_06_25_181040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2021_06_25_174956) do
   create_table "suppliers", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
