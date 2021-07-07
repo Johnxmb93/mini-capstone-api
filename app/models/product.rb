1cc298bceaf5b2d7d174158f7d2f7a4e7838f526
Jonathan Bahena <bahena@Jonathans-MacBook-Air.local>
2021-07-07 11:28:09 -0500

    added order, carted products and some belongs to stuff

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ApplicationRecord
   
   belongs_to :supplier
   has_many :orders
+  has_many :product_categories
+  has_many :categories, through: :product_categories
   def tax
     price * 0.09
   end
